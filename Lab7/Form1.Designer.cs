40fc071e6475be1b91c275896098694bbe9760bd
zhibekm <65110060+zhibekm@users.noreply.github.com>
2021-11-11 23:15:20 +0600

    Add files via upload

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(40, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "First row",
+            "Second row",
+            "Third row",
+            "Fourth row",
+            "Fifth row"});
+            this.comboBox2.Location = new System.Drawing.Point(40, 187);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adding data with method no.1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Adding data with method no.2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(282, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Adding data with method no.3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(282, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(240, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Clear list";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(282, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(240, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Get selected data";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(282, 214);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(240, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Get selected data index";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(282, 243);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(240, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Remove selected line";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(282, 272);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(240, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Remove specific value";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(282, 301);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(240, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Deleting the value by index";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Value 1",
+            "Value 2",
+            "Value 3",
+            "Value 4"});
+            this.comboBox3.Location = new System.Drawing.Point(566, 69);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(222, 21);
+            this.comboBox3.TabIndex = 12;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            this.comboBox3.Click += new System.EventHandler(this.comboBox3_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Value 1",
+            "Value 2",
+            "Value 3",
+            "Value 4"});
+            this.comboBox4.Location = new System.Drawing.Point(566, 185);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(222, 21);
+            this.comboBox4.TabIndex = 13;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            this.comboBox4.Click += new System.EventHandler(this.comboBox4_Click);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Value 1",
+            "Value 2",
+            "Value 3",
+            "Value 4"});
+            this.comboBox5.Location = new System.Drawing.Point(566, 301);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(222, 21);
+            this.comboBox5.TabIndex = 14;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            this.comboBox5.TextChanged += new System.EventHandler(this.comboBox5_TextChanged);
+            this.comboBox5.Click += new System.EventHandler(this.comboBox5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Sorted = true;
+            for (int i = 13; i > 0; i--)
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] sArayItems = {"Excellent", "Good", "Satisfactory", "Unsatisfactory"};
+            comboBox1.Items.Clear();
+            for (int i = 0; i < sArayItems.GetLength(0); i++)
+            {
+                comboBox1.Items.Add(sArayItems[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] sArrayItems;
+            sArrayItems = new string[] {"Excellent", "Good", "Satisfactory", "Unsatisfactory"};
+            comboBox1.DataSource = sArrayItems;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex != -1)
+                MessageBox.Show(comboBox2.SelectedItem.ToString());
+            else
+                MessageBox.Show("You did not select a value", "Error");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex != -1)
+                MessageBox.Show(comboBox2.SelectedIndex.ToString());
+            else
+                MessageBox.Show("You did not select a value", "Error");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Remove(comboBox2.SelectedItem);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Remove("First row");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.RemoveAt(0);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("The SelectedIndexChanged event has occurred", "You clicked the component with the mouse");
+            if (comboBox4.SelectedIndex != -1)
+                MessageBox.Show("You have selected a value" + comboBox4.SelectedItem.ToString());
+            else
+                MessageBox.Show("You did not select a value", "Error");
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Happened Click event ", " You clicked on the component with the mouse ");
+        }
+
+        private void comboBox4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox5_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("The TextChanged event has occurred", "You clicked the component with the mouse");
+            if (comboBox5.SelectedIndex != -1)
+                MessageBox.Show("You have selected a value" + comboBox5.SelectedText);
+            else
+                MessageBox.Show("You did not select a value", "Error");
+        }
+    }
+}
